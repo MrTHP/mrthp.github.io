e63ef91887bd019b7c8178a747e313a8089a2e52
MrTHP <142737708+MrTHP@users.noreply.github.com>
2025-07-05 01:25:02 -0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,5 +1,5 @@
 sudo pacman -S --needed git base-devel \
-  && git clone https://github.com/Gaming-Linux-FR/Architect.git ~/Architect \
+  && git clone https://github.com/MrTHP/Architect.git ~/Architect \
   && cd ~/Architect \
   && chmod +x ./architect.sh \
   && ./architect.sh
